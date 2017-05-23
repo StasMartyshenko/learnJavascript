33762e80b08024b46015a63ff9738e9120ccb62c
StasMartyshenko <martyshenkostas@gmail.com>
2017-05-23 16:32:12 +0300

    if

---
+++ script.js
@@ -0,0 +1,98 @@
+//alert('скрипт');
+
+//var admin;
+//var name;
+//
+//name = 'василий';
+//admin = name;
+//alert(admin);
+
+//var myPlanet = 'Земля';
+//var client = 'Петя';
+
+//var a = 1, b = 1, c = 1;
+//
+//c = ++a; console.log(a); //2
+//d = b++; console.log(d); //1
+//
+//c = (2+ ++a); console.log(c);//5
+//d = (2+ b++); console.log(d);//4
+//
+//console.log(a); //3
+//console.log(b); //3
+
+//var a = 2;
+//
+//var x = 1 + (a *= 2); //5
+
+//var youName = prompt('введите имя', '');
+//alert(youName);
+
+//var js = prompt('название джеса?','');
+//
+//if (js == 'ES') {
+//    alert('true');
+//} else {
+//    alert('false');
+//}
+
+//var number = prompt('вести значение', '');
+//
+//if (number > 0) {
+//    alert('1')
+//} else if (number < 0) {
+//    alert(-1)
+//} else {
+//    alert(0)
+//}
+
+//var login = prompt('login?', '');
+//
+//if (login == 'Админ') {
+//    var pas = prompt('password?', '');
+//    if (pas == 'Чёрный Властелин') {
+//        alert('«Добро пожаловать!»');
+//    } else if (pas == null) {
+//        alert('«Вход отменён»');
+//    } else {
+//        alert('«Пароль неверен»');
+//    }
+//} else if (login == null) {
+//    alert('«Вход отменён»');
+//} else {
+//    alert('Я вас не знаю');
+//}
+
+//result = (a + b < 4) ? "Мало" : "Много";
+
+//var message = (login == 'Вася') ? 'Привет' : (login == 'Директор') ? 'Здраствуйте' : (login == '') ? 'нет логина' : '';
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
