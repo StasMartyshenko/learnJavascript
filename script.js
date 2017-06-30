e58c8ef91f23d51e9502009cad53f29e8e80237d
StasMartyshenko <martyshenkostas@gmail.com>
2017-06-30 18:19:11 +0300

    practic

--- script.js
+++ script.js
@@ -831,42 +831,66 @@
 //
 // console.log(arr2);
 
-var value = 0;
+// ------------------------------------------------------------Сумма через замыкание
+// function sum (a) {
+//   return function (aa) {
+//       console.log(a+aa);
+//   }
+// }
+// sum(1)(2);
+// sum(5)(-1);
 
-function f() {
-  if (1) {
-    value = true;
-  } else {
-    var value = false;
-  }
+// --------------------------------------------------------Функция - строковый буфер
 
-  alert( value );
-}
+// function makeBuffer () {
+//   var counret ='';
+//   return function (name) {
+//       if (arguments.length == 0) {
+//         return counret;
+//       }
+//       counret += name;
+//   }
+//
+// }
+//
+// var buffer = makeBuffer();
+//
+// buffer('Замыкания');
+// buffer(' Использовать');
+// buffer(' Нужно!');
+//
+// alert( buffer() );
 
-f();
+// -------------------------------------------------------Строковый буфер с очисткой
 
+// function makeBuffer() {
+//   var text = '';
+//
+//   function buffer (piece) {
+//     if (arguments.length == 0) { // вызов без аргументов
+//       return text;
+//     }
+//     text += piece;
+//   };
+//
+//   buffer.clear = function() {
+//     text = "";
+//   }
+//
+//   return buffer;
+// };
+//
+// var buffer = makeBuffer();
+//
+// buffer("Тест");
+// buffer(" тебя не съест ");
+// alert( buffer() ); // Тест тебя не съест
+//
+// buffer.clear();
+//
+// alert( buffer() ); // ""
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// ---------------------------------------------------------------------Сортировка
 
 
 
