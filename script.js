3d934e38f4399a7bd08aecb46d431e0e2bf56335
StasMartyshenko <martyshenkostas@gmail.com>
2017-07-12 15:08:27 +0300

    Преобразование объектов: toString и valueOf

--- script.js
+++ script.js
@@ -894,24 +894,23 @@
 
 //-------------------------------------------------------------Создайте калькулятор
 
-var calculator = {
-    read: function() {
-        calculator.a = +prompt('a', '');
-        calculator.b = +prompt('b', '');
-    },
-    sum: function() {
-        return calculator.a + calculator.b;
-    }
-};
-
-calculator.read();
-alert( calculator.sum() );
-console.log(calculator);
+// var calculator = {
+//     read: function () {
+//         this.a = +prompt('a', '');
+//         this.b = +prompt('b', '');
+//     },
+//     sum: function() {
+//         return this.a + this.b;
+//     },
+//     mul: function() {
+//         return this.a * this.b;
+//     }
+//
+// };
+//
+// calculator.read();
+// alert( calculator.sum() );
 // alert( calculator.mul() );
-
-
-
-
 
 
 
